f97c0343c3855f76eedc59a9d5f82fb0ec32d123
Michael Feninets <carpinua@gmail.com>
2018-09-28 17:21:17 +0300

    some corrections

--- Study/myCv/dist/js/main.js
+++ Study/myCv/dist/js/main.js
@@ -1,24 +1,24 @@
-//Select DOM items
+// Select DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-items');
+const navItems = document.querySelectorAll('.nav-item');
 
-//Set Initial State Of menu
+// Set Initial State Of Menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
 
-function toggleMenu(){
-    if(!showMenu) {
+function toggleMenu() {
+    if (!showMenu) {
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
 
-        //Set Menu State
+        // Set Menu State
         showMenu = true;
     } else {
         menuBtn.classList.remove('close');
@@ -27,7 +27,7 @@ function toggleMenu(){
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
-        //Set Menu State
+        // Set Menu State
         showMenu = false;
     }
 }
