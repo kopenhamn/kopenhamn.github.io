51b41005c2d773ca84952891f252bc4025bf6323
Michael Feninets <carpinua@gmail.com>
2018-10-10 14:55:33 +0300

    added menu button and modified logo

--- arcpage/dist/js/main.js
+++ arcpage/dist/js/main.js
@@ -1,1 +1,40 @@
-console.log('NPM Script Test');
+let menuBtn = document.querySelectorAll('.menu-btn');
+let element = document.querySelectorAll('.btnLine');
+let menu = document.querySelectorAll('.menu');
+let main = document.querySelectorAll('main');
+
+let showMenu = false;
+
+
+function openMenu() {
+    if (!showMenu) {
+        //
+        menuBtn[0].classList.add('close');
+        menu[0].classList.add('show');
+        main[0].classList.add('reduce');
+        /*
+        for(x = 0; x < element.length; x++){
+            element[x].classList.add('close');
+        }*/
+
+
+
+
+        showMenu = true;
+
+    } else {
+
+        menuBtn[0].classList.remove('close');
+        menu[0].classList.remove('show');
+        main[0].classList.remove('reduce');
+        
+        /*for(x = 0; x < element.length; x++){
+            element[x].classList.remove('close');
+        }
+        */
+
+
+
+        showMenu = false;
+    }
+}
